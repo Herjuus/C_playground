1826d35d0b716dfa45ad55c7cd782d186c0f5f35
Herjus <herjus420@gmail.com>
2023-10-07 02:08:46 +0200

    ok :(

--- Sdl2/main.cpp
+++ Sdl2/main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
 
 void initializeGame(bool appRunning) {
     appRunning = true;
@@ -13,7 +16,7 @@ int main()
     do {
 
     } while (appRunning);
-        
+
     return 0;
 }
 
