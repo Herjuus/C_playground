de90774e13addbe347cdaff994f85c18da9667f7
Herjus <herjus420@gmail.com>
2023-10-07 11:04:59 +0200

    aight

---
+++ C test/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    char *str = "Hello World!\n";
+
+    int num;
+
+    printf("Enter a number: ");
+
+    scanf("%d", &num);
+
+    printf("You entered: %d\n", num);
+
+    return 0;
+}
--- Sdl2/main.cpp
+++ Sdl2/main.cpp
@@ -1,22 +1,54 @@
-#include <iostream>
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_ttf.h>
+#include <SDL2/SDL.h>
 
-void initializeGame(bool appRunning) {
-    appRunning = true;
-}
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return 1;
+    }
 
-int main()
-{
-    bool appRunning = false;
+    SDL_Window* window = SDL_CreateWindow(
+            "App",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            800,
+            600,
+            SDL_WINDOW_SHOWN
+    );
+    if (window == nullptr) {
+        SDL_Log("Unable to create window: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
 
-    initializeGame(appRunning);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Unable to create renderer: %s", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
 
-    do {
+    SDL_Event event;
+    bool quit = false;
 
-    } while (appRunning);
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
 
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_Rect rect = { 200, 200, 400, 200 };
+        SDL_RenderFillRect(renderer, &rect);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
-
